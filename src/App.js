d35199fe47aa57067f29fb5897bfc4e2f2cd90b2
priyaprafful <priyankamaheshwari.2021@gmail.com>
2018-12-06 12:36:07 +0100

    product-list-done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {Switch,Route,NavLink} from "react-router-dom";
+
+
 import './App.css';
+
+import ProductList from "./components/ProductList.js";
+
+
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Online Shopping</h1>
+          <nav>
+            <NavLink to="/product-list-man">Man</NavLink>
+            <NavLink to="/product-list-women">Women</NavLink>
+          </nav>
+         </header>
+          <Switch>
+            <Route path="/product-list-man"  render={()=><ProductList category="man"/>}/>
+            <Route path="/product-list-women"  render={()=><ProductList category="women"/>}/>
+          </Switch>
       </div>
     );
   }
---
+++ src/components/ProductList.js
@@ -0,0 +1,78 @@
+import React, {Component} from "react";
+import axios from "axios";
+
+
+class ProductList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            productArray:[],
+            filteredProducts:[],
+            category:"",
+         }
+    }
+    componentDidMount(){
+        axios.get("http://localhost:5555/api/products")
+         .then(response=>{
+             //console.log("Product-List",response.data)
+             const filteredProducts=response.data.filter((filterbygender)=>{
+                
+                 return filterbygender.category===this.props.category
+                
+            })
+              this.setState({
+                  filteredProducts:filteredProducts,
+                  productArray:response.data,
+                  category:this.props.category,
+
+                });
+         })
+         .catch(err=>{
+             console.log("ProductList Error", err);
+         })
+    }
+
+    static getDerivedStateFromProps(nextProps,prevState){
+        console.log('nextProps,', nextProps, 'prevState,', prevState)
+        if(nextProps.category!==prevState.category){
+            const {productArray} = prevState;
+            const filteredArray=productArray.filter((filterbygender)=>{
+               return filterbygender.category===nextProps.category
+            })
+            return {
+                filteredProducts:filteredArray,
+                category:nextProps.category,
+            }
+        }
+        return null;
+    } 
+
+    addToCart(productId){
+        console.log("productId",productId);
+        
+    }
+    render() { 
+        const{filteredProducts}= this.state;
+        return ( 
+            <section>
+                <h1>Choose your product</h1>
+                  <ul>
+                      {filteredProducts.map(oneProduct=>{
+                          return(
+                            <li key={oneProduct._id}>
+                              <img src={oneProduct.image} alt={oneProduct.name}/>
+                              <p>{oneProduct.price}</p>
+                              <p>{oneProduct.name}</p>
+                              <p>{oneProduct.brand}</p>
+                              <button onClick={()=>this.addToCart(oneProduct._id)}>Add to cart</button>
+                             
+                            </li>
+                        )
+                     })}
+                </ul>
+          </section>
+         );
+      }
+}
+ 
+export default ProductList;
